871156ee9743b6cdbf6b0c6c0d6f52c8010078f8
Cameron More <123985185+cameronmore@users.noreply.github.com>
2024-12-15 16:44:03 -0500

    updated version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='statsy',
-    version='0.1.1',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         #'numpy>=1.11.1'
