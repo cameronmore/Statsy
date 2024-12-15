a3de0761af5551ee1d95f4334c5ba124a2b0329a
Cameron More <123985185+cameronmore@users.noreply.github.com>
2024-12-15 18:27:52 -0500

    adding basic tests

---
+++ src/tests/test_main.py
@@ -0,0 +1,7 @@
+from statsy import *
+
+def simple_test_summation():
+    assert summation(i=None,range=None,expression=lambda x: x*2,data=[[1,2,3]]) == 12
+
+def complex_test_summation():
+    assert summation(i=2,range=2,expression=lambda y, x, r: x*y*r,data=[[1,2,3],[4,5,6],[2,2,2]]) == 56
