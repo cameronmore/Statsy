70df9e42186a7c05dfa873a7151a7e9cd40119c5
Cameron More <123985185+cameronmore@users.noreply.github.com>
2024-12-16 14:00:27 -0500

    added more content, set up modules

---
+++ src/statsy/Constants.py
@@ -0,0 +1,16 @@
+from .main import *
+
+e = summation(None,None,lambda n: 1/(math.factorial(n)), [[n for n in range (1000)]])
+
+def pi()->Number:
+    """
+    Returns pi
+    """
+    return math.pi
+
+def e_specified(depth:int) -> Number:
+    """
+    A specified depth to calculate the constant e.
+    """
+    depth_list = [n for n in range (depth)]
+    return summation(None,None,lambda n: 1/(math.factorial(n)), [depth_list])
--- src/statsy/__init__.py
+++ src/statsy/__init__.py
@@ -1,1 +1,5 @@
 from .main import *
+from .Conversion import *
+from .Constants import *
+from .Logarithms import *
+from .Methods import *
